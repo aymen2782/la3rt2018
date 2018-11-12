df44dbf85318380529e9de85a3c45053af9747f0
aymen <aymen.sellaouti@gmail.com>
2018-11-12 12:04:48 +0100

    test request response

--- src/Insat/FirstBundle/Controller/DefaultController.php
+++ src/Insat/FirstBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Insat\FirstBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@InsatFirst/Default/index.html.twig');
+        dump($request);
+        $response = new Response('<h1>Bonjour</h1>');
+        return $response;
+        //        return $this->render('@InsatFirst/Default/index.html.twig');
     }
 }
